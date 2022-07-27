9c4c794e87b3b565ac0daf5e8261d396710886da
Isha Hitang <isha.201710@ncit.edu.np>
2022-07-27 13:09:31 -0700

    some js code added

--- external.js
+++ external.js
@@ -33,3 +33,77 @@ function DigitalClock(){
    digitalclock.innerHTML = hrs + " : " + min + " : " + sec + " " + diem;
 }
 DigitalClock();
+
+
+var start = document.getElementById('start');
+var stop = document.getElementById('stop');
+var reset = document.getElementById('reset');
+
+var wm = document.getElementById('f_min');
+var ws = document.getElementById('f_sec');
+
+var bm = document.getElementById('b_min');
+var bs = document.getElementById('b_sec');
+
+var focusTimer;
+/* to start timing */
+start.addEventListener('click', function(){
+   if(focusTimer === undefined){
+    focusTimer = setInterval(timer, 1000);
+   }
+   else{
+    alert("Timer is already running");
+   }
+})
+/*to reset timing */
+reset.addEventListener('click', function(){
+   wm.innerText=25;
+   ws.innerText="00";
+
+   bm.innerText=5;
+   bs.innerText="00";
+   document.getElementById('counter').innerText = 0;
+   stopInterval();
+   startTimer=undefined;
+ })
+
+ /*to stop timing */
+stop.addEventListener('click', function(){
+    stopInterval();
+    startTimer=undefined;
+})
+
+function timer(){
+    //focus timer countdown
+    if(ws.innerText !=0){
+        ws.innerText--;
+    }else if(wm.innerText !=0 && ws.innerText==0){
+        ws.innerText = 59;
+        wm.innerText--;
+    }
+
+    //break timer countdown
+    if(wm.innerText==0&&ws.innerText==0){
+        if(bs.innerText !=0 ){
+            bs.innerText--;
+        }else if(bm.innerText !=0 && bs.innerText ==0){
+            bs.innerText = 59;
+            bs.innerText--;
+        }
+    }
+
+
+    //increment counter by one if one full cycle is completed
+    if(wm.innerHTML == 0 && ws.innerHTML==0 && bm.innerHTML==0){
+       wm.innerHTML = 25;
+       ws.innerHTML = "00";
+
+       bm.innerHTML = 5;
+       bs.innerHTML = "00";
+
+       document.getElementsById('counter').innerHTML++;
+    }
+}
+function stopInterval(){
+    clearInterval(focusTimer);
+}
